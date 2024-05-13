1f303c83bfef5662770a0032566d9c61a26da86a
Sam Sharma <samsharma.dev@gmail.com>
2024-05-12 19:12:10 -0700

    added useState too get the first video and a function to set the selected videe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
+import { useState } from 'react';
 import './App.scss';
 import Header from './components/Header/Header.jsx';
 import VideoPlayer from './components/VideoPlayer/VideoPlayer.jsx';
 import videoDetails from './data/video-details.json';
 function App() {
+  const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
+
   return (
     <div className="App">
       <Header />
-      <VideoPlayer />
+      <VideoPlayer selectedVideo={selectedVideo}/>
     </div>
   );
 }
