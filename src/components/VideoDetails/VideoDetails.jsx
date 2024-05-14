134da42991469c548c104e78b922054ed0fb0e6d
Sam Sharma <samsharma.dev@gmail.com>
2024-05-13 22:05:00 -0700

    converted timestamp to date

--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -2,13 +2,14 @@ import viewsIcon from '../../assets/icons/views.svg';
 import likeIcon from '../../assets/icons/likes.svg';
 import "./VideoDetails.scss";
 const VideoDetails = props => {
+    const date = (new Date(props.selectedVideo.timestamp)).toLocaleString();
     return(
         <section className="video-details">
             <h1 className="video-details__heading">{props.selectedVideo.title}</h1>
             <div className="video-details__container">
                 <div className="video-details__info">
                     <p className="video-details__channel">By {props.selectedVideo.channel}</p>
-                    <p className="video-details__date">Posted {props.selectedVideo.timestamp}</p>
+                    <p className="video-details__date">Posted {date}</p>
                 </div>
                 <div className="video-details__numbers">
                     <div className="video-details__views">
