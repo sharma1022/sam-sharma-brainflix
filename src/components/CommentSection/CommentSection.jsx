de90ee5cd90b8d0bd87ca026f16dac51750f2e52
Sam Sharma <samsharma.dev@gmail.com>
2024-05-14 21:31:14 -0700

    added initial styling for comment form

--- src/components/CommentSection/CommentSection.jsx
+++ src/components/CommentSection/CommentSection.jsx
@@ -1,17 +1,18 @@
 import mohanImg from '../../assets/images/Mohan-muruge.jpg';
-
+import './CommentSection.scss';
 const CommentSection = ({ selectedVidComments }) => {
 
     return (
         <section className="comments">
             <p className="comments__count">{selectedVidComments.length} Comments</p>
             <div className="comments__top">
-                <img className='comments__avatar' src={mohanImg} alt="Mohan Avatar" />
+                <div className='comments__avatar'></div>
                 <form className='comments__form'>
                     <div className='comments__form-container'>
-                        <label className='comemnts__label' htmlFor="comment">Join the Conversation</label>
+                        <label className='comments__label' htmlFor="comment">Join the Conversation</label>
                         <textarea className='comments__input' name="comment" id="comment" placeholder='Add a new comment'></textarea>
                     </div>
+                    <button>Comment</button>
                 </form>
             </div>
         </section>
