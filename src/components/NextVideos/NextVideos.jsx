e82240b4f4651a55a31b8a48c4240fae3aa170bf
Sam Sharma <samsharma.dev@gmail.com>
2024-05-29 19:46:48 -0700

    Added basic jsx for the NextVideos section

---
+++ src/components/NextVideos/NextVideos.jsx
@@ -0,0 +1,14 @@
+import './NextVideos.scss';
+
+const NextVideos = () => {
+    return (
+        <section className="next-videos">
+            <h3 className="next-videos__header">Next Videos</h3>
+            <ul className="next-videos__list">
+                
+            </ul>
+        </section>
+    );
+}
+
+export default NextVideos;
