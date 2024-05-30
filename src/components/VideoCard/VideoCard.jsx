3acf970814b304df592cca39dc64de8fca435a80
Sam Sharma <samsharma.dev@gmail.com>
2024-05-29 20:33:10 -0700

    added basic jsx for the Video Card

--- src/components/VideoCard/VideoCard.jsx
+++ src/components/VideoCard/VideoCard.jsx
@@ -1,1 +1,15 @@
-const VideoCard = ({videos})
+const VideoCard = ({videos}) => {
+    return (
+        <li className="next-videos__item">
+        <div className="next-videos__img-container">
+            <img className="next-videos__img" src={videos.thumbnail} alt={`${props.title} video.`} />
+        </div>
+        <div className="next-videos__details">
+            <p className="next-videos__title">{videos.title}</p>
+            <p className="next-videos__channel">{videos.channel}</p>
+        </div>
+        </li>
+    );
+}
+
+export default VideoCard;
