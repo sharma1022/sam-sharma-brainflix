9c7ddf43d6a96d26c89b3afc1cf07c35cc349c20
Sam Sharma <samsharma.dev@gmail.com>
2024-06-02 20:20:43 -0700

    made a few styling changes

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -2,6 +2,8 @@ import "./VideoPlayer.scss";
 
 const VideoPlayer = ({ selectedVideo }) => {
   const image= selectedVideo.image;
+  //to scroll to top when rerendering
+  window.scrollTo(0, 0);
   return (
     <div className="video">
       <video
--- src/utils/timeSince.jsx
+++ src/utils/timeSince.jsx
@@ -2,7 +2,7 @@ const timeSince = (date) => {
     var seconds = Math.floor((new Date() - date) / 1000);
   
     var interval = seconds / 31536000;
-  
+    
     if (interval > 1) {
       return Math.floor(interval) + " years ago";
     }
