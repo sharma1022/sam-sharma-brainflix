bb35729acc840319e9ff6e614f6fa8db86dad0d7
Sam Sharma <samsharma.dev@gmail.com>
2024-06-08 20:42:02 -0700

    moved the code from app.jsx to MainVideoPage.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,12 @@
 import { useState } from "react";
 import "./App.scss";
 import Header from "./components/Header/Header.jsx";
-import VideoPlayer from "./components/VideoPlayer/VideoPlayer.jsx";
-import videoDetails from "./data/video-details.json";
-import VideoDetails from "./components/VideoDetails/VideoDetails.jsx";
-import CommentSection from "./components/CommentSection/CommentSection.jsx";
-import videoList from "./data/videos.json";
-import NextVideos from "./components/NextVideos/NextVideos.jsx";
+
 
 function App() {
-  const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
-  const handleVideoClick = (videoId) => {
-    const clickedVideo = videoDetails.find((video) => video.id === videoId);
-    console.log(videoId);
-    console.log(clickedVideo);
-    setSelectedVideo(clickedVideo);
-    
-  };
-  return (
-    <div className="App">
-      <Header />
-      <main>
-        <VideoPlayer selectedVideo={selectedVideo} />
-        <section className="page-content">
-          <div className="page-content__selected-vid">
-            <VideoDetails selectedVideo={selectedVideo} />
-            <CommentSection selectedVideo={selectedVideo} />
-          </div>
-          <NextVideos
-            selectedVideo={selectedVideo}
-            handleVideoClick={handleVideoClick}
-          />
-        </section>
-      </main>
-    </div>
-  );
+  return(
+    <Header />
+  )
 }
 
 export default App;
---
+++ src/pages/MainVideoPage/MainVideoPage.jsx
@@ -0,0 +1,41 @@
+import { useState } from "react";
+import Header from "../../components/Header/Header.jsx";
+import VideoPlayer from "../../components/VideoPlayer/VideoPlayer.jsx";
+import videoDetails from "../../data/video-details.json";
+import VideoDetails from "../../components/VideoDetails/VideoDetails.jsx";
+import CommentSection from "../../components/CommentSection/CommentSection.jsx";
+import videoList from "./data/videos.json";
+import NextVideos from "./components/NextVideos/NextVideos.jsx";
+import "./App.scss";
+
+const MainVideoPage = () => {
+    
+    const [selectedVideo, setSelectedVideo] = useState(videoDetails[0]);
+
+    const handleVideoClick = (videoId) => {
+      const clickedVideo = videoDetails.find((video) => video.id === videoId);
+      console.log(videoId);
+      console.log(clickedVideo);
+      setSelectedVideo(clickedVideo);
+      
+    };
+    return (
+      <div className="App">
+        <main>
+          <VideoPlayer selectedVideo={selectedVideo} />
+          <section className="page-content">
+            <div className="page-content__selected-vid">
+              <VideoDetails selectedVideo={selectedVideo} />
+              <CommentSection selectedVideo={selectedVideo} />
+            </div>
+            <NextVideos
+              selectedVideo={selectedVideo}
+              handleVideoClick={handleVideoClick}
+            />
+          </section>
+        </main>
+      </div>
+    );
+}
+
+export default MainVideoPage;
