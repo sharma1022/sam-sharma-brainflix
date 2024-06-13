e72716029d7c6db72e8608d365ae8994b1a449e9
Sam Sharma <samsharma.dev@gmail.com>
2024-06-12 18:17:22 -0700

    added initial code for form submit

--- src/pages/VideoUploadPage/VideoUploadPage.jsx
+++ src/pages/VideoUploadPage/VideoUploadPage.jsx
@@ -1,14 +1,29 @@
 import "./VideoUploadPage.scss"
 import uploadImg from "../../assets/images/Upload-video-preview.jpg";
 import Button from "../../components/Button/Button";
+import { useState } from "react";
 
 const VideoUploadPage = () => {
 
+    const [vidTitle, setVidTitle] = useState("");
+    const [vidDesc, setVidDesc] = useState("");
+
+    const handleTitleChange = (e) => {
+        setVidTitle(e.target.value);
+    };
+
+    const handleDescChange = (e) => {
+        setVidDesc(e.target.value);
+    }
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+    }
     return(
         <main>
             <section className="upload-video">
                 <h1 className="upload-video__heading">Upload Video</h1>
-                <form action="submit" className="upload-video__form">
+                <form action="submit" className="upload-video__form" onSubmit={handleSubmit}>
                     <div className="upload-video__container">
                         <div className="upload-video__left">
                             <p className="upload-video__thumbnail-label">Video Thumbnail</p>
@@ -16,9 +31,9 @@ const VideoUploadPage = () => {
                         </div>
                         <div className="upload-video__right">
                             <label htmlFor="" className="upload-video__label">Title your video</label>
-                            <input type="text" className="upload-video__input" placeholder="Add a title to your video" name="videoTitle" id="videoTitle"/>
+                            <input type="text" className="upload-video__input" value={vidTitle} placeholder="Add a title to your video" name="vidTitle" id="vidTitle" onChange={handleTitleChange}/>
                             <label htmlFor="" className="upload-video__label">Add a Video Description</label>
-                            <textarea className="upload-video__input upload-video__input--desc" name="videoDesc" id="videoDesc" placeholder="Add a description to your video" ></textarea>
+                            <textarea className="upload-video__input upload-video__input--desc" value={vidDesc} name="vidDesc" id="vidDesc" placeholder="Add a description to your video" onChange={handleDescChange}></textarea>
                         </div>
                     </div>
                     <div className="upload-video__form-actions">
