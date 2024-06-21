2ee23ee55582837e9f26532fd4249a6cc5106dcc
Sam Sharma <samsharma.dev@gmail.com>
2024-06-20 21:49:02 -0700

    added basic styling and jsx to not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "./App.scss";
 import Header from "./components/Header/Header.jsx";
 import MainVideoPage from "./pages/MainVideoPage/MainVideoPage.jsx";
 import VideoUploadPage from "./pages/VideoUploadPage/VideoUploadPage.jsx";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/home" element={<Navigate to="/" />} />
         <Route path="/upload" element={<VideoUploadPage />} />
         <Route path="/videos/:videoId" element={<MainVideoPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,22 @@
+import errorImg from '../../assets/images/404error.svg';
+import Button from "../../components/Button/Button";
+import "./NotFoundPage.scss";
+
+const NotFoundPage = () => {
+    return(
+        <>
+        <div className="not-found">
+            <img className='not-found__img' src={errorImg} alt="404 Error Image" />
+            <div className="not-found__container">
+                <h1 className='not-found__heading'>Page Not Found</h1>
+                <p className='not-found__text'>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+                <Button type="button"
+                className="button--return"
+                text="Back to Homepage"/>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default NotFoundPage;
